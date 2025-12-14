87a17ccd21a6d4d8bec3f6aee32ede9e810e3bf0
Nyytsuu <Babianoraileer@gmail.com>
2025-12-15 01:22:05 +0800

    typingg done?

---
+++ Typing Quiz/Script.js
@@ -0,0 +1,72 @@
+const questions = [
+  {
+    q: "What is the primary purpose of a router in a home network?",
+    answers: [
+      "to forward data between the home network and the internet",
+      "forward data between your home network and the internet"
+    ]
+  },
+  {
+    q: "Which network device is used to amplify a Wi-Fi signal to extend the coverage area of a wireless network?",
+    answers: [
+      "wi-fi repeater",
+      "wifi repeater",
+      "wi-fi extender",
+      "wifi extender"
+    ]
+  },
+  {
+    q: "What does the acronym LAN commonly stand for?",
+    answers: ["local area network"]
+  }
+];
+let current = 0;
+let score = 0;
+
+loadQuestion();
+
+function loadQuestion() {
+  const question = questions[current];
+
+  document.getElementById("question-text").textContent = question.q;
+  updateProgress(current + 1, questions.length);
+
+  const typingBox = document.getElementById("typing-container");
+  const input = document.getElementById("typing-answer");
+
+  typingBox.style.display = "block";
+  input.value = "";
+  input.focus();
+
+  input.onkeydown = (e) => {
+    if (e.key === "Enter") {
+      checkAnswer(input.value, question.answers);
+    }
+  };
+}
+
+function checkAnswer(userInput, correctAnswers) {
+  const userAns = userInput.trim().toLowerCase();
+
+  if (correctAnswers.includes(userAns)) {
+    score++;
+  }
+
+  nextQuestion();
+}
+
+function nextQuestion() {
+  current++;
+
+  if (current < questions.length) {
+    loadQuestion();
+  } else {
+    localStorage.setItem("score", score);
+    window.location.href = "result.html";
+  }
+}
+
+function updateProgress(current, total) {
+  const progress = document.getElementById("progress");
+  progress.style.width = (current / total) * 100 + "%";
+}
